c73578da26a6ef6935a11d4ae7cd06427cda2ad6
Thomas Dehaeze <dehaeze.thomas@gmail.com>
2020-09-18 09:29:51 +0200

    Add Stewart Platform Model

---
+++ stewart_platform/drone_platform_DataFile.m
@@ -0,0 +1,156 @@
+% Simscape(TM) Multibody(TM) version: 5.1
+
+% This is a model data file derived from a Simscape Multibody Import XML file using the smimport function.
+% The data in this file sets the block parameter values in an imported Simscape Multibody model.
+% For more information on this file, see the smimport function help page in the Simscape Multibody documentation.
+% You can modify numerical values, but avoid any other changes to this file.
+% Do not add code to this file. Do not edit the physical units shown in comments.
+
+%%%VariableName:smiData
+
+
+%============= RigidTransform =============%
+
+%Initialize the RigidTransform structure array by filling in null values.
+smiData.RigidTransform(12).translation = [0.0 0.0 0.0];
+smiData.RigidTransform(12).angle = 0.0;
+smiData.RigidTransform(12).axis = [0.0 0.0 0.0];
+smiData.RigidTransform(12).ID = '';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(1).translation = [17.500000000021 30.310889132419 5.1574606296500001];  % mm
+smiData.RigidTransform(1).angle = 0.093902078374528131;  % rad
+smiData.RigidTransform(1).axis = [0 0 1];
+smiData.RigidTransform(1).ID = 'AssemblyGround[Drone_Stewart_Platform_V8 voice-coil.CATProduct-1:Spring cylinder.CATPart-1]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(2).translation = [0 0 0];  % mm
+smiData.RigidTransform(2).angle = 0;  % rad
+smiData.RigidTransform(2).axis = [0 0 0];
+smiData.RigidTransform(2).ID = 'AssemblyGround[Drone_Stewart_Platform_V8 voice-coil.CATProduct-1:Upper_Stewart_Platform_V8.CATPart-1]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(3).translation = [17.500000000021 -30.310889132492001 5.1574606296500001];  % mm
+smiData.RigidTransform(3).angle = 0.093902078374528131;  % rad
+smiData.RigidTransform(3).axis = [0 0 1];
+smiData.RigidTransform(3).ID = 'AssemblyGround[Drone_Stewart_Platform_V8 voice-coil.CATProduct-1:Spring cylinder.CATPart-3]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(4).translation = [9.6240451994409995 -30.811470883506999 19.383957937758002];  % mm
+smiData.RigidTransform(4).angle = 3.1415926535897931;  % rad
+smiData.RigidTransform(4).axis = [0.88807383397699313 6.3170900042374556e-16 -0.45970084338121897];
+smiData.RigidTransform(4).ID = 'AssemblyGround[Drone_Stewart_Platform_V8 voice-coil.CATProduct-1:Voice-coil.CATPart-1]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(5).translation = [21.871493913361999 -23.740403071639001 19.383957937758002];  % mm
+smiData.RigidTransform(5).angle = 2.6777446800421498;  % rad
+smiData.RigidTransform(5).axis = [-0.79025275363749503 -0.45625264004045041 -0.40906492617245471];
+smiData.RigidTransform(5).ID = 'AssemblyGround[Drone_Stewart_Platform_V8 voice-coil.CATProduct-1:Voice-coil.CATPart-2]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(6).translation = [21.871493913361 23.740403071566 19.383957937758002];  % mm
+smiData.RigidTransform(6).angle = 2.3226757410894345;  % rad
+smiData.RigidTransform(6).axis = [0.4840501729431676 0.83839949295006799 -0.25056280708588496];
+smiData.RigidTransform(6).ID = 'AssemblyGround[Drone_Stewart_Platform_V8 voice-coil.CATProduct-1:Voice-coil.CATPart-3]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(7).translation = [9.6240451994409995 30.811470883434001 19.383957937758002];  % mm
+smiData.RigidTransform(7).angle = 2.1862760354647519;  % rad
+smiData.RigidTransform(7).axis = [5.6189562004407762e-16 -1 -1.3924588923326856e-16];
+smiData.RigidTransform(7).ID = 'AssemblyGround[Drone_Stewart_Platform_V8 voice-coil.CATProduct-1:Voice-coil.CATPart-4]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(8).translation = [-31.495539112739003 7.0710678118320001 19.383957937758002];  % mm
+smiData.RigidTransform(8).angle = 2.3226757410894345;  % rad
+smiData.RigidTransform(8).axis = [-0.48405017294316754 0.83839949295006766 0.25056280708588635];
+smiData.RigidTransform(8).ID = 'AssemblyGround[Drone_Stewart_Platform_V8 voice-coil.CATProduct-1:Voice-coil.CATPart-5]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(9).translation = [-31.495539112739003 -7.0710678119050003 19.383957937758002];  % mm
+smiData.RigidTransform(9).angle = 2.6777446800421507;  % rad
+smiData.RigidTransform(9).axis = [0.79025275363749525 -0.45625264004045002 0.40906492617245482];
+smiData.RigidTransform(9).ID = 'AssemblyGround[Drone_Stewart_Platform_V8 voice-coil.CATProduct-1:Voice-coil.CATPart-6]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(10).translation = [0 0 0];  % mm
+smiData.RigidTransform(10).angle = 0;  % rad
+smiData.RigidTransform(10).axis = [0 0 0];
+smiData.RigidTransform(10).ID = 'AssemblyGround[Drone_Stewart_Platform_V8 voice-coil.CATProduct-1:Bottom_Stewart_Platform_V8.CATPart-1]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(11).translation = [-34.999999999978996 -3.7000000000000001e-11 5.1574606296500001];  % mm
+smiData.RigidTransform(11).angle = 0.093902078374528131;  % rad
+smiData.RigidTransform(11).axis = [0 0 1];
+smiData.RigidTransform(11).ID = 'AssemblyGround[Drone_Stewart_Platform_V8 voice-coil.CATProduct-1:Spring cylinder.CATPart-2]';
+
+%Translation Method - Cartesian
+%Rotation Method - Arbitrary Axis
+smiData.RigidTransform(12).translation = [0 0 0];  % mm
+smiData.RigidTransform(12).angle = 0;  % rad
+smiData.RigidTransform(12).axis = [0 0 0];
+smiData.RigidTransform(12).ID = 'RootGround[Drone_Stewart_Platform_V8 voice-coil.CATProduct-1]';
+
+
+%============= Solid =============%
+%Center of Mass (CoM) %Moments of Inertia (MoI) %Product of Inertia (PoI)
+
+%Initialize the Solid structure array by filling in null values.
+smiData.Solid(4).mass = 0.0;
+smiData.Solid(4).CoM = [0.0 0.0 0.0];
+smiData.Solid(4).MoI = [0.0 0.0 0.0];
+smiData.Solid(4).PoI = [0.0 0.0 0.0];
+smiData.Solid(4).color = [0.0 0.0 0.0];
+smiData.Solid(4).opacity = 0.0;
+smiData.Solid(4).ID = '';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(1).mass = 0.002953024625510399;  % kg
+smiData.Solid(1).CoM = [0.00019528918640542437 -0.0030390869707000701 3.4103846069334764];  % mm
+smiData.Solid(1).MoI = [0.094245235034989605 0.095111221695610218 0.060564976663331278];  % kg*mm^2
+smiData.Solid(1).PoI = [9.2822238167834426e-07 0.0015998098386190715 -2.9934446424201115e-05];  % kg*mm^2
+smiData.Solid(1).color = [0.82352941176470584 0.82352941176470584 1];
+smiData.Solid(1).opacity = 1;
+smiData.Solid(1).ID = 'Spring cylinder.CATPart*:*Default';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(2).mass = 0.032807859410420866;  % kg
+smiData.Solid(2).CoM = [4.3204111866222267e-05 6.4929928363256894e-06 22.029956321592298];  % mm
+smiData.Solid(2).MoI = [10.958807269683136 10.958651478878741 21.418645161274547];  % kg*mm^2
+smiData.Solid(2).PoI = [-2.7272640778847972e-06 2.0116845742236026e-06 2.0925824216820959e-06];  % kg*mm^2
+smiData.Solid(2).color = [0 1 1];
+smiData.Solid(2).opacity = 1;
+smiData.Solid(2).ID = 'Upper_Stewart_Platform_V8.CATPart*:*Default';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(3).mass = 0.00072851613140055473;  % kg
+smiData.Solid(3).CoM = [1.6672752107505068e-07 0.049264195897497115 6.8426699166844651];  % mm
+smiData.Solid(3).MoI = [0.023642069482559629 0.023409631588225184 0.0076150552208997047];  % kg*mm^2
+smiData.Solid(3).PoI = [0.00019174011014260877 5.6635742238393906e-10 -6.4605353421422721e-10];  % kg*mm^2
+smiData.Solid(3).color = [0.82352941176470584 0.82352941176470584 1];
+smiData.Solid(3).opacity = 1;
+smiData.Solid(3).ID = 'Voice-coil.CATPart*:*Default';
+
+%Inertia Type - Custom
+%Visual Properties - Simple
+smiData.Solid(4).mass = 0.036808422349281396;  % kg
+smiData.Solid(4).CoM = [1.5151381609408909e-08 -2.5842738425576439e-08 3.8488764942888207];  % mm
+smiData.Solid(4).MoI = [16.573140175725058 16.573140156158694 32.475141872846649];  % kg*mm^2
+smiData.Solid(4).PoI = [4.9976421207227284e-09 -2.8421200098863714e-09 -1.3395036264003659e-08];  % kg*mm^2
+smiData.Solid(4).color = [1 1 0];
+smiData.Solid(4).opacity = 1;
+smiData.Solid(4).ID = 'Bottom_Stewart_Platform_V8.CATPart*:*Default';
+
